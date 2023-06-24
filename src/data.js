fbf37a75e6df5c6c6044332f68e4f8f659f9cbce
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-06-24 04:26:50 +0100

    optimizing perfomaance

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const products = [
     {
         id: 1,
         img: require('../src/assets/admin-panel.png'),
-        link: 'https://github.com/emmanuelomejua/Ecommerce-Admin-dashboard-templete-design.git',
+        link: 'https://taupe-crumble-3c44b2.netlify.app/',
     },
     {
         id: 2,
@@ -12,7 +12,7 @@ export const products = [
     {
         id: 3,
         img: require('../src/assets/netflix-clone-logo.png'),
-        link: 'https://goggle.com',
+        link: 'https://github.com/emmanuelomejua/Netflix-clone-with-mern.git',
     },
     {
         id: 4,
@@ -22,11 +22,11 @@ export const products = [
     {
         id: 5,
         img: require('../src/assets/weda.png'),
-        link: 'https://goggle.com',
+        link: 'https://dapper-gumption-71d744.netlify.app/',
     },
     {
         id: 6,
         img:  require('../src/assets/blog-app-logo.png'),
-        link: 'https://goggle.com',
+        link: 'https://github.com/emmanuelomejua/Naija-blog-complete-project.git',
     }
 ]
